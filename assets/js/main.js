3407033ba25deb0e6a1be4f4a217a14930659a74
ApharMagaramova <meherremova.afer2003@gmail.com>
2022-11-03 17:14:38 +0400

    sort, adding with enter completed, dragrop began

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,13 +13,21 @@ function addItem() {
     li.append(inputText);
 
     li.append(document.querySelector('.dl-btn').cloneNode(true));
-
     ul.append(li);
+    inputText.autofocus = true;
+
+    addEventsDragAndDrop(li);
 }
 
 addButton.addEventListener("click", addItem);
 
-input.addEventListener('keypressed', addItem);
+ul.addEventListener('keypress', (event) => {
+    let keyCode = event.keyCode ? event.keyCode : event.which;
+    // 13 points the enter key
+    if (keyCode === 13) {
+        addButton.click();
+    }
+});
 
 //   deleting
 ul.addEventListener("click", (e) => {
@@ -37,10 +45,12 @@ let order = false;
 
 sortBtn.addEventListener('click', () => {
 
+    // change picture on mouse click
     if (!order) {
-        document.getElementById('change').src = '/assets/Images/desc_img.png';
+        document.getElementById('change').src = './assets/Images/desc-hover.png';
+
     } else {
-        document.getElementById('change').src = '/assets/Images/asc_img.png';
+        document.getElementById('change').src = './assets/Images/asc-hover.png';
     }
 
     const list = ul.children;
@@ -92,4 +102,67 @@ sortBtn.addEventListener('click', () => {
         list[i].firstElementChild.value = todoItems[i];
     }
 });
+
+
+
+// drag and drop functionality
+var remove = document.querySelector('.draggable');
+
+function dragStart(e) {
+    this.style.opacity = '0.4';
+    dragSrcEl = this;
+    e.dataTransfer.effectAllowed = 'move';
+    e.dataTransfer.setData('text/html', this.innerHTML);
+    console.log('drag start')
+};
+
+function dragEnter(e) {
+    this.classList.add('over');
+    console.log('drag Enter')
+}
+
+function dragLeave(e) {
+    e.stopPropagation();
+    this.classList.remove('over');
+    console.log('drag Leave')
+}
+
+function dragOver(e) {
+    e.preventDefault();
+    e.dataTransfer.dropEffect = 'move';
+    console.log('drag over')
+    return false;
+}
+
+function dragDrop(e) {
+    if (dragSrcEl != this) {
+        dragSrcEl.innerHTML = this.innerHTML;
+        this.innerHTML = e.dataTransfer.getData('text/html');
+    }
+    console.log('drag drop')
+    return false;
+}
+
+function dragEnd(e) {
+    var listItens = document.querySelectorAll('.draggable');
+    [].forEach.call(listItens, function (item) {
+        item.classList.remove('over');
+    });
+    this.style.opacity = '1';
+}
+
+function addEventsDragAndDrop(el) {
+    el.addEventListener('dragstart', dragStart, false);
+    el.addEventListener('dragenter', dragEnter, false);
+    el.addEventListener('dragover', dragOver, false);
+    el.addEventListener('dragleave', dragLeave, false);
+    el.addEventListener('drop', dragDrop, false);
+    el.addEventListener('dragend', dragEnd, false);
+}
+
+var listItens = document.querySelectorAll('.draggable');
+[].forEach.call(listItens, function (item) {
+    addEventsDragAndDrop(item);
+});
+
 
